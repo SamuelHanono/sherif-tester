731cb1fbe8f48385b8432338a42d0b7991a607cb
SamuelHanono <67659304+SamuelHanono@users.noreply.github.com>
2026-01-21 23:38:02 +0000

    Tester function

---
+++ main.py
@@ -0,0 +1,9 @@
+# The goal of this function is to return the numbers 1-10, order does not matter, in an array.
+
+def printer():
+    arr = []
+    for i in range(11):
+        arr.append(i)
+    return arr
+
+print(printer())

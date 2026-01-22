7604d90039fbfdb1bfb83bb697d59f53f349e826
SamuelHanono <67659304+SamuelHanono@users.noreply.github.com>
2026-01-22 09:34:59 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 # The goal of this function is to return the numbers 1-10, order does not matter, in an array.
 def printer():
     arr = []
-    for i in range(1, 11):
+    for i in range(11):
         arr.append(i)
     return arr
 

ed01b25274218977a1af2db0f66984c2ab07f974
SamuelHanono <67659304+SamuelHanono@users.noreply.github.com>
2026-01-21 21:37:56 -0500

    Modify printerNeg to return negative numbers
    Update printerNeg function to return numbers from -1 to -10.

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 # The goal of this function is to return the numbers 1-10, order does not matter, in an array.
-
 def printer():
     arr = []
     for i in range(11):
         arr.append(i)
     return arr
-    
+
+# The goal of this function is to return the numbers from -1 to -10, order does not matter, in an array.
 def printerNeg():
     arr = []
     for i in range(11):
